7ae4b0ad5e6e5bbdc1e9d6503e6f95149137effa
Catalin Dumitru <colin.dumitru@gmail.com>
2014-10-01 20:21:00 +0300

    modified receipts to git pull the app source code

--- deploy_python/recipes/default.rb
+++ deploy_python/recipes/default.rb
@@ -1,7 +1,21 @@
-bash "process_places_requests" do
-	user "root"
-	cwd "/opt/worker"
-	code <<-EOH
-	python2.7 application.py
-	EOH
+node[:deploy].each do |application, deploy_config|
+
+	app_dir = "/opt/#{deploy_config[:application]}"
+
+	git app_dir  do
+		repository deploy_config[:scm][:repository]
+		action :sync
+		notifies :run, "bash[compile_app_name]"
+	end
+
+	bash "run_app" do
+		user "root"
+		cwd app_dir
+		code <<-EOH
+		python2.7 application.py
+		EOH
+	end
+	
 end
+
+
--- setup_python/recipes/default.rb
+++ setup_python/recipes/default.rb
@@ -1,15 +1,3 @@
 package "python2.7" do
   action :install
 end
-
-directory "/opt/worker" do
-    mode 0755
-    action :create
-end
-
-node[:deploy].each do |application, deploy|
-	link deploy[:deploy_to] do
-		to "/opt/worker"
-		action :create
-	end
-end
