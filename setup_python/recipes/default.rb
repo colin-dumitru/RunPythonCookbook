1ca9a88e9e7fcac83836e84dbe3a7a1fd3fe8545
Catalin Dumitru <colin.dumitru@gmail.com>
2014-10-01 20:59:19 +0300

    installing boto during setup

--- setup_python/recipes/default.rb
+++ setup_python/recipes/default.rb
@@ -1,3 +1,15 @@
 package "python2.7" do
   action :install
 end
+
+package "python-pip" do
+  action :install
+end
+
+bash "install_boto" do
+	user "root"
+	cwd app_dir
+	code <<-EOH
+	pip install boto
+	EOH
+end
